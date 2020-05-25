518ec7034ac9a7615fc7148486b91106b26c866c
alicepozd <57236582+alicepozd@users.noreply.github.com>
2020-05-25 16:05:04 +0300

    DSU into class

--- 3_2.cpp
+++ 3_2.cpp
@@ -2,21 +2,32 @@
 #include <vector>
 #include <algorithm>
 
-int find_set(std::vector<int>& comp, int vertex){
-    while (comp[vertex] != -1){
-        vertex = comp[vertex];
+class DSU{
+public:
+    DSU(int n) {
+        comp.resize(n, -1);
+        comp_size.resize(n, 1);
     }
-    return vertex;
-}
 
-void union_set(int second_set, int first_set, std::vector<int>& comp, std::vector<int>& comp_size){
-    if (first_set > second_set) {
-        std::swap(first_set, second_set);
+    int find_set(int vertex){
+        while (comp[vertex] != -1){
+            vertex = comp[vertex];
+        }
+        return vertex;
     }
-    comp[first_set] = second_set;
-    comp_size[second_set] += comp_size[first_set];
-    comp_size[first_set] = 0;
-}
+
+    void union_set(int second_set, int first_set){
+        if (first_set > second_set) {
+            std::swap(first_set, second_set);
+        }
+        comp[first_set] = second_set;
+        comp_size[second_set] += comp_size[first_set];
+        comp_size[first_set] = 0;
+    }
+private:
+    std::vector<int> comp;
+    std::vector<int> comp_size;
+};
 
 bool edges_compare(const std::pair<std::pair<int, int>, int>& first, const std::pair<std::pair<int, int>, int>& second){
     return first.second < second.second;
@@ -24,16 +35,15 @@ bool edges_compare(const std::pair<std::pair<int, int>, int>& first, const std::
 
 int kruskal_weight(int n, std::vector<std::pair<std::pair<int, int>, int>>& edges){
     std::sort(edges.begin(), edges.end(), edges_compare);
-    std::vector<int> comp(n, -1);
-    std::vector<int> comp_size(n, 1);
+    DSU set_system = DSU(n);
     int eadges_in_MST = 0;
     int weight = 0;
     int eadge_num = 0;
     while (eadges_in_MST < n - 1){
-        int first_set = find_set(comp, edges[eadge_num].first.first);
-        int second_set = find_set(comp, edges[eadge_num].first.second);
+        int first_set = set_system.find_set(edges[eadge_num].first.first);
+        int second_set = set_system.find_set(edges[eadge_num].first.second);
         if (first_set != second_set){
-            union_set(second_set, first_set, comp, comp_size);
+            set_system.union_set(second_set, first_set);
             weight += edges[eadge_num].second;
             eadges_in_MST++;
         }
@@ -51,7 +61,7 @@ int main() {
         std::cin >> b >> e >> w;
         b--;
         e--;
-        edges.push_back(std::make_pair(std::make_pair(e, b), w));
+        edges.emplace_back(std::make_pair(std::make_pair(e, b), w));
     }
     std::cout << kruskal_weight(n, edges);
 }
